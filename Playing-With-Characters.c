81d35e37ba30807e6eed1d2283ab293721db3992
Aaditya Jaiswal <78849620+AADI-1331@users.noreply.github.com>
2021-09-08 13:59:08 +0530

    Create Playing-With-Characters.c

---
+++ Playing-With-Characters.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+char ch;
+    int MAX_LEN=256;
+    char s[MAX_LEN];
+    char sen[MAX_LEN];
+
+    //charachter
+    
+    scanf("%c ",&ch);
+    scanf("%s ",s);
+    fgets(sen, MAX_LEN, stdin);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+    
+    
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
